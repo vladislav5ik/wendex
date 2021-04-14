264c45c5311e39a7dfab03955d41d060611c6c17
vladislav5ik <spigin.vlad@mail.ru>
2021-04-14 22:16:23 +0300

    solved issue with relative/absolute paths in clion project. Database files moved in another folder

---
+++ cmake-build-debug/CMakeFiles/homework4.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Professional/CLionProjects/homework4/Address.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/Address.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/Car.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/Car.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/Driver.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/Driver.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/DriverGateway.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/DriverGateway.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/Gateway.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/Gateway.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/Order.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/Order.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/Passenger.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/Passenger.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/PassengerGateway.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/PassengerGateway.cpp.obj"
+  "C:/Users/Professional/CLionProjects/homework4/main.cpp" "C:/Users/Professional/CLionProjects/homework4/cmake-build-debug/CMakeFiles/homework4.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/homework4.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework4.dir/Address.cpp.obj"
+  "CMakeFiles/homework4.dir/Car.cpp.obj"
+  "CMakeFiles/homework4.dir/Driver.cpp.obj"
+  "CMakeFiles/homework4.dir/DriverGateway.cpp.obj"
+  "CMakeFiles/homework4.dir/Gateway.cpp.obj"
+  "CMakeFiles/homework4.dir/Order.cpp.obj"
+  "CMakeFiles/homework4.dir/Passenger.cpp.obj"
+  "CMakeFiles/homework4.dir/PassengerGateway.cpp.obj"
+  "CMakeFiles/homework4.dir/main.cpp.obj"
+  "homework4.exe"
+  "homework4.exe.manifest"
+  "homework4.pdb"
+  "libhomework4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
